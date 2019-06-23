b866248b0b2e1ce00b734708e465c462693604d7
Adam Sarli <adam.sarli@gmail.com>
2019-06-22 21:41:55 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,28 @@
 class Post < ActiveRecord::Base
 
-  validate :is_title_case 
-  before_validation :make_title_case 
+  validate :is_title_case
+  before_validation :make_title_case
   belongs_to :author
 
-  #put new code here
+  def self.by_author(author_id)
+    self.where(author: author_id)
+  end
+
+  def self.by_date(date)
+    if date == "Today"
+      self.where("created_at >=?", Time.zone.today.beginning_of_day)
+    else
+      self.where("created_at <?", Time.zone.today.beginning_of_day)
+    end
+  end
+
+  def self.from_today
+    where("created_at >=?", Time.zone.today.beginning_of_day)
+  end
+
+  def self.old_news
+    where("created_at <?", Time.zone.today.beginning_of_day)
+  end
 
   private
 
