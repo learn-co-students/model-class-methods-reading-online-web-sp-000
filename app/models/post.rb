358c8c26ce7443925317f11af66d78c3081677ef
Chinedu Arinze <c.s.arinze@gmail.com>
2020-02-05 14:07:49 -0500

    All test are passing

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,20 @@ class Post < ActiveRecord::Base
 
   #put new code here
 
+  def self.by_author(author_id) 
+    where(author: author_id)
+  end
+
+  def self.from_today
+    where("created_at >=?", Time.zone.today.beginning_of_day)
+  end
+
+  def self.old_news
+    where("created_at <?", Time.zone.today.beginning_of_day)
+  end
+
+    @posts = Post.where("created_at >=?", Time.zone.today.beginning_of_day)
+
   private
 
   def is_title_case
