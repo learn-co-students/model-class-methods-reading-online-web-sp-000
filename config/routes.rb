79d1fa79156d159c354ddbf449faa66aeba9fdd3
lukesherwood <luke.sherwood55@gmail.com>
2020-04-22 15:07:08 +1200

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  resources :posts, :authors
+  resources :authors, only: [:show] do
+    # nested resource for posts
+    resources :posts, only: [:show, :index]
+  end
+ 
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+ 
+  root 'posts#index'
 end
